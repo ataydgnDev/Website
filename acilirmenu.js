8ef6bab3e175206d8b7fca0ab7e36e29dd488b2c
Ata yavuz Aydoğan <137526893+ataydgnDev@users.noreply.github.com>
2023-06-24 01:31:03 +0300

    Create acilirmenu.js

---
+++ acilirmenu.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var mLk1 = document.getElementById("mLk1");
+  var mCt1 = document.getElementById("mCt1");
+
+  mLk1.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mCt1.style.display === "block") {
+      mCt1.style.display = "none";
+    } else {
+      mCt1.style.display = "block";
+    }
+  });
+
+  var mLk2 = document.getElementById("mLk2");
+  var mCt2 = document.getElementById("mCt2");
+
+  mLk2.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mCt2.style.display === "block") {
+      mCt2.style.display = "none";
+    } else {
+      mCt2.style.display = "block";
+    }
+  });
+
+  var mLk3 = document.getElementById("mLk3");
+  var mCt3 = document.getElementById("mCt3");
+
+  mLk3.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mCt3.style.display === "block") {
+      mCt3.style.display = "none";
+    } else {
+      mCt3.style.display = "block";
+    }
+  });
+
+  var mLk4 = document.getElementById("mLk4");
+  var mCt4 = document.getElementById("mCt4");
+
+  mLk4.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mCt4.style.display === "block") {
+      mCt4.style.display = "none";
+    } else {
+      mCt4.style.display = "block";
+    }
+  });
+
+  var mlk5 = document.getElementById("mlk5");
+  var mct5 = document.getElementById("mct5");
+
+  mlk5.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mct5.style.display === "block") {
+      mct5.style.display = "none";
+    } else {
+      mct5.style.display = "block";
+    }
+  });
+  var mlk6 = document.getElementById("mlk6");
+  var mct6 = document.getElementById("mct6");
+
+  mlk6.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (mct6.style.display === "block") {
+      mct6.style.display = "none";
+    } else {
+      mct6.style.display = "block";
+    }
+  });
+});
+
